933ee3b1ffda1f8e266a7950683e20161bf17b3a
Belinda Vennam <beemarie@us.ibm.com>
2018-02-13 15:32:57 -0600

    updating nodejs 8 code

--- runtimes/nodejs-8/actions/process-change.js
+++ runtimes/nodejs-8/actions/process-change.js
@@ -1,12 +1,12 @@
-function main(params) {
+function main({name, color}) {
   return new Promise(function(resolve, reject) {
-    if (!params.name || !params.color) {
+    if (!name || !color) {
       reject({
         'error': 'Please make sure name and color are passed in as params.'
       });
       return;
     } else {
-      var message = 'A ' + params.color + ' cat named ' + params.name + ' was added.';
+      var message = 'A ' + color + ' cat named ' + name + ' was added.';
       console.log(message);
       resolve({
         change: message
