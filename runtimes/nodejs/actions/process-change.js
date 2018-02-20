e36c40cb2e3e04d080892d4e4bb37c7be4283821
Belinda Vennam <beemarie@us.ibm.com>
2018-02-20 11:22:42 -0800

    update runtimes folders

--- runtimes/nodejs/actions/process-change.js
+++ runtimes/nodejs/actions/process-change.js
@@ -1,17 +1,17 @@
 function main(params) {
-  return new Promise(function(resolve, reject) {
-    if (!params.name || !params.color) {
+  const { name, color } = params;
+  return new Promise((resolve, reject) => {
+    if (!name || !color) {
       reject({
-        'error': 'Please make sure name and color are passed in as params.'
+        error: 'Please make sure name and color are passed in as params.'
       });
-      return;
     } else {
-      var message = 'A ' + params.color + ' cat named ' + params.name + ' was added.';
+      const message = `A ${color} cat named ${name} was added`;
       console.log(message);
       resolve({
-        change: message
+        change: message,
       });
-      return;
     }
   });
 }
+exports.main = main;
