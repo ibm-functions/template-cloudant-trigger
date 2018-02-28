3aeb1b2676a81162eadde3d82755a8b2fdf6e89f
Belinda Vennam <beemarie@us.ibm.com>
2018-02-28 12:20:49 -0600

    update to match ibm functions repo

--- runtimes/nodejs/actions/process-change.js
+++ runtimes/nodejs/actions/process-change.js
@@ -1,17 +1,17 @@
 function main(params) {
-  const { name, color } = params;
-  return new Promise((resolve, reject) => {
-    if (!name || !color) {
+  return new Promise(function(resolve, reject) {
+    if (!params.name || !params.color) {
       reject({
-        error: 'Please make sure name and color are passed in as params.'
+        'error': 'Please make sure name and color are passed in as params.'
       });
+      return;
     } else {
-      const message = `A ${color} cat named ${name} was added`;
+      var message = 'A ' + params.color + ' cat named ' + params.name + ' was added.';
       console.log(message);
       resolve({
-        change: message,
+        change: message
       });
+      return;
     }
   });
 }
-exports.main = main;
