3636b7a465d0cf1063532ab77896e9dcec37244f
Belinda Vennam <beemarie@us.ibm.com>
2018-04-02 16:20:21 -0500

    jshint not actually used

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -4,10 +4,6 @@ SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 HOMEDIR="$SCRIPTDIR/../../../"
 DEPLOYDIR="$HOMEDIR/openwhisk/catalog/extra-packages/packageDeploy"
 
-# jshint support
-sudo apt-get -y install nodejs npm
-sudo npm install -g jshint
-
 # clone utilties repo. in order to run scanCode.py
 cd $HOMEDIR
 git clone https://github.com/apache/incubator-openwhisk-utilities.git
